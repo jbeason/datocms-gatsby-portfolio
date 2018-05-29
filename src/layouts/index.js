20eb22a396e0cd3fd3f338aa676a3574af736e02
jbeason <joe@basement47.com>
2018-05-29 13:10:27 -0400

    HelmetDatoCms defined

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
+import { HelmetDatoCms } from 'gatsby-source-datocms'
 
 import Navbar from '../components/Navbar'
 import './all.sass'
